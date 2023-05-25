01e999058710e63ac0bb6706dc48b4b36fd74e0b
iqwa1107 <ramahs49@gmail.com>
2023-05-25 20:31:25 +0700

    menampilkan dislay

--- Doublelinkdlist_128/Doublelinkdlist_128.cpp
+++ Doublelinkdlist_128/Doublelinkdlist_128.cpp
@@ -188,3 +188,34 @@ int main() {
 			cout << "\nMenu" << endl;
 			cout << "1. add a record to the list" << endl;
 			cout << "2. delete a record from the list" << endl;
+			cout << "3. View all records in the ascending order of roll numbers" << endl;
+			cout << "4. View all records in the descending order of roll numbers" << endl;
+			cout << "5. Search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "/nEnter your choice (1-6):";
+			char ch;
+			cin >> ch;
+
+			switch (ch) {
+			case '1':
+				obj.addNode();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.traverse();
+				break;
+			case '4':
+				obj.revtraverse();
+				break;
+			case '5':
+				obj.searchData();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
+			}
+		}
