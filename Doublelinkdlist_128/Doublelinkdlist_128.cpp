671a1c7932f1e894eaccce616846ab8fe904e304
iqwa1107 <ramahs49@gmail.com>
2023-05-25 15:32:38 +0700

    class DoubleLinkedList

--- Doublelinkdlist_128/Doublelinkdlist_128.cpp
+++ Doublelinkdlist_128/Doublelinkdlist_128.cpp
@@ -10,3 +10,19 @@ public:
 	node* prev;
 };
 
+class DoubleLinkedList {
+
+private:
+	node* START;
+
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, node** preivious, node** curreent);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
+};
+
