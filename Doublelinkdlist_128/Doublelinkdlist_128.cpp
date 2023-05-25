b9511de0235b714525b415354198d01b406e6ff4
iqwa1107 <ramahs49@gmail.com>
2023-05-25 16:05:44 +0700

    membuat unsert step 1 sampai 7

--- Doublelinkdlist_128/Doublelinkdlist_128.cpp
+++ Doublelinkdlist_128/Doublelinkdlist_128.cpp
@@ -52,10 +52,32 @@ void DoubleLinkedList::addNode() {
 		newnode->next = START; //step 3 
 		if (START != NULL)
 			START->prev = newnode; //step 4
-		newnode ->prev = NULL; //step 4
+		newnode ->prev = NULL; //step 5
+		START = newnode;//step 6
+		return;
+	}
 
+	/*insert a node batween tow nodes in the list*/
+	node* current = START; // stap 1.a
+	node* previous = NULL; // stap 1.b
+	while (current->next !=NULL && current->next->noMhs < nim) // step 1.c
+	{
+		previous = current; //1.d
+		current = current->next; //1.e
 
 	}
 
+	if (current->next != NULL && nim == current -> noMhs) 
+	{
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
+	newnode->next = current; // steep 4
+	newnode->prev = current; // step 5
+	if (current->next != NULL)
+		current->next->prev = newnode; //step6
+	current->next = newnode; //step 7
 
 }
+
+
